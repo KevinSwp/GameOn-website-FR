96c154f5f8f344962279e5f4dd01b19333040252
KevinSWP <thranva@gmail.com>
2022-10-31 11:15:45 +0100

    check form almost done

--- starterOnly/js/form.js
+++ starterOnly/js/form.js
@@ -1,14 +1,14 @@
-//################## Minimum age ##################
+//################## MINIMUM AGE ##################
 const minAge = 13;
 
-//################## Message if errors ##################
-const messageName = "2 caractères minimum. Chiffre, espace, caractère spécial non autorisé. ";
+//################## MESSAGE IF ERRORS ##################
+const messageName = "2 lettres minimum. Chiffre, espace, caractère spécial non autorisé. ";
 const messageEmail = "Adresse E-mail invalide (exemple@yahoo.com).";
-const messageBirthday = "Veuillez saisir une date valide et avoir minimum 13 ans.";
+const messageBirthday = "Veuillez saisir une date valide (age min. 13 ans).";
 const tournamentMessage = "Veuillez saisir un nombre (max.99).";
 const cityMessage = "Veuillez indiquer une ville";
 
-//################## DOM Elements ##################
+//################## DOM ELEMENTS ##################
 // First name
 const firstName = document.getElementById("first");
 const firstNameError = document.querySelector("#first + p");
@@ -21,11 +21,14 @@ const emailError = document.querySelector("#email + p");
 // Birthdate
 const birthdate = document.getElementById("birthdate");
 const birthdateError = document.querySelector("#birthdate + p");
-//tournament
+// Tournament
 const tournamentQuantity = document.getElementById("quantity");
 const tournamentQuantityError = document.querySelector("#quantity + p");
+// City
+const locationRadio = document.querySelectorAll("input[name='location']");
+const locationError = document.querySelector("#checkbox1 + p");
 
-//################## Ckeckers ##################
+//################## CHECKERS ##################
 // Check if the name has minimum 2 characters, whithout number, without space and special character.
 const validName = (name) => {
     const regex = /^[a-zA-Z]+[a-zA-Z-]*[a-zA-Z]$/;
@@ -44,7 +47,7 @@ const validEmail = (email) => {
 const validBirthdate = (birthdate) => {
     const date = new Date(birthdate);
     const now = Date.now();
-    const yearMilliseconds = 365.25 * 24 * 60 * 60 * 1000;
+    const yearMilliseconds = 365 * 24 * 60 * 60 * 1000;
     const age = (now - date) / yearMilliseconds;
 
     return age >= minAge;
@@ -52,10 +55,31 @@ const validBirthdate = (birthdate) => {
 
 // Check quantity
 const validQuantity = () => {
-    return ((tournamentQuantity.value) >= 0 && (tournamentQuantity.value) <= 99);
+    return (
+        (tournamentQuantity.value) >= 0 &&
+        (tournamentQuantity.value) <= 99 &&
+        (tournamentQuantity.value) !== ""
+    );
 };
 
-// ################## Add/remove error message ##################
+// Check radio
+const validRadio = () => {
+    document.querySelector("input[name='location']:checked");
+} 
+
+// Check form
+// const validForm = () => {
+//     return (
+//         validName(firstName.value) &&
+//         validName(lastName.value) &&
+//         validEmail(email.value) &&
+//         validBirthdate(birthdate.value) &&
+//         validQuantity(tournamentQuantity.value) &&
+//         validRadio(locationRadio) !== null
+//     );
+// };
+
+// ################## ADD/REMOVE ERRORS MESSAGES ##################
 // Add/remove error message of first name/last name
 const nameValidity = (inputField, errorField) => {
     if (validName(inputField.value)) {
@@ -75,54 +99,69 @@ const nameValidity = (inputField, errorField) => {
 // Add/remove error message of email
 const emailValidity = (emailField, errorField) => {
     if (validEmail(emailField.value)) {
-      emailField.classList.remove("border-red");
-  
-      errorField.textContent = "";
-      errorField.classList.remove("text-red");
+        emailField.classList.remove("border-red");
+    
+        errorField.textContent = "";
+        errorField.classList.remove("text-red");
     } 
     else {
-      emailField.classList.add("border-red");
-  
-      errorField.textContent = messageEmail;
-      errorField.classList.add("text-red");
+        emailField.classList.add("border-red");
+    
+        errorField.textContent = messageEmail;
+        errorField.classList.add("text-red");
     }
 };
 
 // Add/remove error message of birthdate
 const birthdateValidity = (birthdateField, errorField) => {
     if (validBirthdate(birthdateField.value)) {
-      birthdateField.classList.remove("border-red");
-  
-      errorField.textContent = "";
-      errorField.classList.remove("text-red");
-    } else {
-      birthdateField.classList.add("border-red");
-  
-      errorField.textContent = messageBirthday;
-      errorField.classList.add("text-red");
+        birthdateField.classList.remove("border-red");
+    
+        errorField.textContent = "";
+        errorField.classList.remove("text-red");
+    }
+    else {
+        birthdateField.classList.add("border-red");
+    
+        errorField.textContent = messageBirthday;
+        errorField.classList.add("text-red");
     }
 };
 
 // Add/remove error message of tournament
 const quantityValidity = (quantityField, errorField) => {
-    if (validQuantity(tournamentQuantity.value)) {
-      quantityField.classList.remove("border-red");
-  
-      errorField.textContent = "";
-      errorField.classList.remove("text-red");
-    } else {
+    if (validQuantity(quantityField.value)) {
+        quantityField.classList.remove("border-red");
+    
+        errorField.textContent = "";
+        errorField.classList.remove("text-red");
+    }
+    else {
         quantityField.classList.add("border-red");
   
-      errorField.textContent = tournamentMessage;
-      errorField.classList.add("text-red");
+        errorField.textContent = tournamentMessage;
+        errorField.classList.add("text-red");
     }
 };
 
-// ################## Check input field ##################
+// Add/remove error message of location
+const locationValidity = (errorField) => {
+    if (validRadio (errorField)) {
+
+        errorField.textContent = "";
+        errorField.classList.remove("text-red");
+    }
+    else {
+        errorField.textContent = cityMessage;
+        errorField.classList.add("text-red");
+    }
+};
+
+// ################## CHECK INPUT FIELD ##################
 // Check first name validity
 firstName.oninput = () => {
     nameValidity(firstName, firstNameError);
-  };
+};
 firstName.addEventListener = () => {
     nameValidity(firstName, firstNameError);
 };
@@ -130,7 +169,7 @@ firstName.addEventListener = () => {
 // Check last name validity
 lastName.oninput = () => {
     nameValidity(lastName, lastNameError);
-  };
+};
 lastName.addEventListener = () => {
     nameValidity(lastName, lastNameError);
 };  
@@ -138,7 +177,7 @@ lastName.addEventListener = () => {
 // Check email validity:
 email.oninput = () => {
     emailValidity(email, emailError);
-  };
+};
 email.addEventListener = () => {
     emailValidity(email, emailError);
 };
@@ -146,16 +185,32 @@ email.addEventListener = () => {
 // Check birthdate validity:
 birthdate.oninput = () => {
     birthdateValidity(birthdate, birthdateError);
-  };
+};
 birthdate.addEventListener = () => {
     birthdateValidity(birthdate, birthdateError);
 };
 
 // Check tournament validity
+tournamentQuantity.oninput = () => {
+    quantityValidity(tournamentQuantity,tournamentQuantityError);
+};
 tournamentQuantity.addEventListener = () => {
     quantityValidity(tournamentQuantity,tournamentQuantityError);
 };
 
-tournamentQuantity.onchange = () => {
-    quantityValidity(tournamentQuantity,tournamentQuantityError);
+// Check radio validity
+tournamentQuantity.addEventListener = () => {
+    locationValidity(tournamentQuantityError);
+};
+
+// Check form
+const checkForm = document.getElementById("modal-form").onsubmit = (event) => {
+    event.preventDefault();
+
+    nameValidity(firstName, firstNameError);
+    nameValidity(lastName, lastNameError);
+    emailValidity(email, emailError);
+    birthdateValidity(birthdate, birthdateError);
+    quantityValidity(tournamentQuantity, tournamentQuantityError);
+    locationValidity(locationError);
 };
